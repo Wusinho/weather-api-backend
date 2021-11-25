f9e745544b7f9eaabc885bb7e2121e936de5dcd2
Heber Lazo <heberlazo@gmail.com>
2021-11-25 16:47:54 -0500

    Added model validation tests

--- spec/models/weather_spec.rb
+++ spec/models/weather_spec.rb
@@ -1,5 +1,17 @@
 require 'rails_helper'
 
 RSpec.describe Weather, type: :model do
-  
+  describe 'validations' do
+    it 'validates presence of required field' do
+      should validate_presence_of(:date)
+      should validate_presence_of(:weather_index)
+    end
+
+    it 'validate realtionships' do
+      should have_one(:location)
+      should have_many(:temperatures)
+      should accept_nested_attributes_for(:location)
+    end
+
+  end
 end
